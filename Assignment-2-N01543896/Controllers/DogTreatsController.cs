6e08c5e2714e5bd63a3770fb28ee8dac802282d3
Vaibhav Baria <kiboyagami0@gmail.com>
2024-02-22 13:33:34 -0500

    added J1 and J2 problems

---
+++ Assignment-2-N01543896/Controllers/DogTreatsController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment_2_N01543896.Controllers
+{
+    public class DogTreatsController : ApiController
+    {
+        /// <summary>
+        /// Determines the dog's happiness level based on the types and quantities of treats given.
+        /// This J1 problem is from the 2020 Canadian Computing Competition (CCC).
+        /// </summary>
+        /// <param name="smallTreats">Number of small treats given.</param>
+        /// <param name="medTreats">Number of medium treats given.</param>
+        /// <param name="largeTreats">Number of large treats given.</param>
+        /// <returns>A string indicating the dog's happiness level ("Happy" or "Sad").</returns>
+        /// <example>
+        /// GET /api/J1/DogTreats/3/1/0 -> "The dog is Sad"
+        /// </example> 
+        /// <example>
+        /// GET /api/J1/DogTreats/3/2/1 -> "The dog is Happy"
+        /// </example>
+        [HttpGet]
+        [Route("api/J1/DogTreats/{smallTreats}/{medTreats}/{largeTreats}")]
+        public string HappinessMeter(int smallTreats, int medTreats, int largeTreats)
+        {
+            int sTreatHappiness = smallTreats * 1;
+            int mTreatHappiness = medTreats * 2;
+            int lTreatHappiness = largeTreats * 3;
+
+            int happiness = sTreatHappiness + mTreatHappiness + lTreatHappiness;
+
+            string state;
+
+            if (happiness >= 10)
+            {
+                state = "Happy";
+            }
+            else
+            {
+                state = "Sad";
+            }
+
+            string response = "The dog is " + state;
+
+            return response;
+        }
+    }
+}
---
+++ Assignment-2-N01543896/Controllers/EpidemiologyController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment_2_N01543896.Controllers
+{
+    public class EpidemiologyController : ApiController
+    {
+        /// <summary>
+        /// Simulates a simple disease spread model to determine the day on which the total number of infected people
+        /// surpasses a given threshold.
+        /// This J2 problem is from the 2020 Canadian Computing Competition (CCC).
+        /// </summary>
+        /// <param name="p">The threshold number of people who must be infected (P < 10).</param>
+        /// <param name="n">The number of people initially infected on Day 0 (N < P & N < R).</param>
+        /// <param name="r">The number of people each infected person infects per day (R < 10).</param>
+        /// <returns>A string stating the day on which "p" people are infected.</returns>
+        /// <example>
+        /// GET /api/J2/DiseaseSpreadModel/750/1/5 -> "Threshold reached on day 4"
+        /// </example>
+        [HttpGet]
+        [Route("api/J2/DiseaseSpreadModel/{P}/{N}/{R}")]
+        public string DiseaseSpreadModel(int p, int n, int r)
+        {
+            int currentSpread = n;
+            int daysCounter = 0;
+            while (currentSpread <= p)
+            {
+                currentSpread += n * r;
+                n = n * r;
+                daysCounter++;
+                //Debug.WriteLine(currentSpread);
+            }
+
+            string response = "Threshold reached on day " + daysCounter;
+            return response;
+        }
+    }
+}
